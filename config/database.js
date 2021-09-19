aac8bd99b20d9729ad5ddb9efd352a806ff2ac4b
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-09-19 20:35:53 -0300

    :hammer: update .env

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'reactjs.6e03n.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'react_landing_pages'),
-        username: env('DATABASE_USERNAME', 'react_landing_pages_admin'),
-        password: env('DATABASE_PASSWORD', 'react_landing_pages_password_123'),
+        host: env('DATABASE_HOST'),
+        srv: env.bool('DATABASE_SRV'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
